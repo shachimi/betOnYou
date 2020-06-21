0003f1dca51106b31a3b28e05b9a59b6ca7ba9be
Samir Hachimi <hachimi.samir@gmail.com>
2020-06-21 23:55:24 +0200

    Add Dockerfile and clean init file

--- betOnYou/src/flaskr/__init__.py
+++ betOnYou/src/flaskr/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 from . import api, db, utils
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -13,12 +13,8 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py')
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py')
 
     # ensure the instance folder exists
     try:
@@ -30,16 +26,5 @@ def create_app(test_config=None):
     utils.init_app(app)
 
     app.register_blueprint(api.blueprint)
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        c = db.get_db().cursor()
-        c.execute('select * from player')
-        result = c.fetchall()
-        print(result)
-        print(result)
-        print(app.config['API_SECRET'])
-        return 'Hello, World! %s' % result
 
     return app
